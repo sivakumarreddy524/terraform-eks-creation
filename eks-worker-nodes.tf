25bfd4d800222dc427baae0990c0eff34ee99f2f
Siva Kumar Reddy <sivakumarreddy524@gmail.com>
2020-04-20 19:51:36 +0530

    fluentd yaml added

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -38,11 +38,17 @@ resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryR
   role       = aws_iam_role.demo-node.name
 }
 
+resource "aws_iam_role_policy_attachment" "demo-node-AmazonCloudWatch" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+  role       = aws_iam_role.demo-node.name
+}
+
 resource "aws_eks_node_group" "demo" {
   cluster_name    = aws_eks_cluster.demo.name
   node_group_name = "demo"
   node_role_arn   = aws_iam_role.demo-node.arn
   subnet_ids      = aws_subnet.demo[*].id
+
 
   scaling_config {
     desired_size = 1
@@ -54,5 +60,6 @@ resource "aws_eks_node_group" "demo" {
     aws_iam_role_policy_attachment.demo-node-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.demo-node-AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.demo-node-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.demo-node-AmazonCloudWatch,
   ]
 }
